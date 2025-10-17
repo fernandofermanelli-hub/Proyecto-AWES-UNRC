68bec0311739423d2b821f8488cb675f0884bbf3
fernandofermanelli-hub <fernandofermanelli@gmail.com>
2025-10-17 11:46:09 -0300

    Add files via upload

---
+++ Kites/geometry.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parametros
+parametros = pd.read_json('./parametros.json')
+ancho = 2
+largo = 3
+superficie = 4
+radius = 5
